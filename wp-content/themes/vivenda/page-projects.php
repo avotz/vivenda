2b0d5986d9e164cb0aecd34b1a6857824edbff6c
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-11-08 15:25:21 -0600

    cambios page projects

---
+++ wp-content/themes/vivenda/page-projects.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Page Projects
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package naturalia
+ */
+
+get_header(); ?>
+ <?php get_template_part( 'template-parts/content', 'banner' ); ?>
+
+ 	
+        
+  <?php get_template_part( 'template-parts/content', 'projects-filters' ); ?>
+
+ 
+
+<?php /*get_sidebar();*/ ?>
+<?php get_footer(); ?>
